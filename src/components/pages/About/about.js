5a39f87eca2f20d5331255309cd3eb03d54197cd
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-11-25 22:44:29 +0600

    new commit

---
+++ src/components/pages/About/about.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import NavigationBar from '../../assets/navbar/navbar';
+import FooterBar from '../../assets/footer/footer';
+
+class About extends Component{
+    render(){
+        return(
+            <div>
+                <NavigationBar/>
+                <h1>Hello This is about page</h1>
+                <FooterBar/>
+            </div>
+            
+        )
+    }
+}
+
+export default About;
--- src/router.js
+++ src/router.js
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 
 import Home from "./components/pages/Home/home";
+import About from './components/pages/About/about';
 const Rounting = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/about" component={About}/>
             </Switch>
         </Router>
     )
