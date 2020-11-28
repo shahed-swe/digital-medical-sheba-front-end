f24bec520a5817eeaad7e0b0be883355a440d981
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-11-28 12:47:24 +0600

    new commit

--- src/components/assets/navbar/navbar.jsx
+++ src/components/assets/navbar/navbar.jsx
@@ -14,8 +14,6 @@ class NavigationBar extends Component{
         this.leftSidebarObj.classList.toggle('show');
     }
 
-
-
     render(){
 
         return(
@@ -25,33 +23,32 @@ class NavigationBar extends Component{
                         <div className="col-12 d-flex flex-wrap justify-content-between align-items-center newnav">
                             <div className="site-branding d-flex align-items-center">
                                 <a className="d-block" href="/" rel="home"><img className="d-block" src={Logo} alt="logo"/></a>
-                        </div>
-
+                            </div>
                             <nav className="site-navigation d-flex justify-content-end align-items-center" ref={Sidebar => this.leftSidebarObj = Sidebar} type={this.type} created={this.onCreate}>
-                                    <ul className="d-flex flex-column flex-lg-row justify-content-lg-end align-items-center">
-                                        <li className="current-menu-item"><a href="/">Home</a></li>
-                                        <li className=""><a href="/about">About</a></li>
-                                        <li className=""><a href="/services">Services</a></li>
-                                        <li className=""><a href="/contact">Contact</a></li>
-                                        <li className=""><a href="/login">Login</a></li>
-                                        <li className=""><a href="/registration">Registration</a></li>
+                                <ul className="d-flex flex-column flex-lg-row justify-content-lg-end align-items-center">
+                                    <li className=""><a href="/">Home</a></li>
+                                    <li className=""><a href="/about">About</a></li>
+                                    <li className=""><a href="/services">Services</a></li>
+                                    <li className=""><a href="/contact">Contact</a></li>
+                                    <li className=""><a href="/login">Login</a></li>
+                                    <li className=""><a href="/registration">Registration</a></li>
 
-                                        <li className="call-btn button gradient-bg mt-3 mt-md-0">
-                                            <a className="d-flex justify-content-center align-items-center" href="#call"><img src={Emergency} alt="_"/> +88 017 621 78238</a>
-                                </li>
-                            </ul>
-                        </nav>
+                                    <li className="call-btn button gradient-bg mt-3 mt-md-0">
+                                        <a className="d-flex justify-content-center align-items-center" href="#call"><img src={Emergency} alt="_"/> +88 017 621 78238</a>
+                                    </li>
+                                </ul>
+                            </nav>
 
                             <div className="hamburger-menu d-lg-none" onClick={this.leftToggle} >
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
